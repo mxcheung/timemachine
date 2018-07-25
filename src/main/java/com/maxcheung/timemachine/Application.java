be1d266c6069287466b39f99f51a1e17deefa142
max cheung <maxcheung@optusnet.com.au>
2018-07-25 22:19:00 +1000

    initial commit

---
+++ src/main/java/com/maxcheung/timemachine/Application.java
@@ -0,0 +1,18 @@
+package com.maxcheung.timemachine;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	private static final Logger LOG = LoggerFactory.getLogger(Application.class);
+
+	public static void main(String[] args) {
+		LOG.info("Application starting ..." );
+		SpringApplication.run(Application.class, args);
+	}
+
+}
