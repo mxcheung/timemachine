d4d463a655af7bc3342f31acf8bf216c4021c304
max cheung <maxcheung@optusnet.com.au>
2018-08-10 05:28:58 +1000

    use running clock at

--- src/test/java/com/maxcheung/timemachine/service/TimeMachineTest.java
+++ src/test/java/com/maxcheung/timemachine/service/TimeMachineTest.java
@@ -1,8 +1,11 @@
 package com.maxcheung.timemachine.service;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -19,11 +22,23 @@ public class TimeMachineTest {
 	}
 
 	@Test
-	public void test() {
+	public void testUseFixedClockAt() throws InterruptedException {
 	    TimeMachineService timeMachineService = new TimeMachineServiceImpl();
 	    timeMachineService.useFixedClockAt(twoWeeksAgo);
 	    assertEquals(twoWeeksAgo.toLocalDate(), timeMachineService.getToday());
+	    TimeUnit.MILLISECONDS.sleep(10);
+	    assertEquals(0, timeMachineService.now().until( twoWeeksAgo, ChronoUnit.MILLIS));
+	    assertEquals(twoWeeksAgo, timeMachineService.now());
 	    timeMachineService.useSystemDefaultZoneClock();
 	    assertEquals(now.toLocalDate(), timeMachineService.getToday());
 	}
+	
+	@Test
+	public void testUseRunningClockAt() throws InterruptedException {
+	    TimeMachineService timeMachineService = new TimeMachineServiceImpl();
+	    timeMachineService.useRunningClockAt(twoWeeksAgo);
+	    TimeUnit.MILLISECONDS.sleep(10);
+	    assertNotEquals(0, timeMachineService.now().until( twoWeeksAgo, ChronoUnit.MILLIS));
+	}
+
 }
